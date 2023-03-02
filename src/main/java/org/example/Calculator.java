dce5a38defed8ef95f22f6e47c5234079a651a79
Artem Peshkov <a.peshkov@fstan.ru>
2023-03-02 13:11:08 +0100

    redone with OOP

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,20 +2,48 @@ package org.example;
 
 //Using a Test Driven and Object Oriented approach, create a simple String Calculator which covers the following requirements:
 //        1. adds numbers present in the input, e.g "1,2" = 3
-//        2. treats empty or null input as zero, e.g "" = 0, null = 0 - done
-//        3. supports different delimiters, e.g "1,2,3", "1 2 3" - done
-//        4. does not support negative numbers - done
-//        5. ignores numbers greater than 100 - done
+//        2. treats empty or null input as zero, e.g "" = 0, null = 0
+//        3. supports different delimiters, e.g "1,2,3", "1 2 3"
+//        4. does not support negative numbers
+//        5. ignores numbers greater than 100
 //        Java SE (any version) and JUnit (any version) are the only libraries allowed.
 
-
 public class Calculator {
-    public static void main(String[] args) {
+    //value for calculation
+    private String valueForCalc;
+
+    //result of calculation
+    private int result;
+
+    //constructor with assigning value for calculation
+    public Calculator(String valueForCalc) {
+        this.valueForCalc = valueForCalc;
     }
 
-    public static int plus(String source) {
-        if (source == null || source.equals("")) return 0;
-        String[] arrayOfNumbers = source.split("[^-a-zA-Z\\d]");
+    //default constructor
+    public Calculator() {
+    }
+
+    //getter of value for calculation
+    public String getValueForCalc() {
+        return valueForCalc;
+    }
+
+    //setter for changing value for calculation
+    public void setValueForCalc(String valueForCalc) {
+        this.valueForCalc = valueForCalc;
+    }
+
+    //getter for result. Calculation start here
+    public int getResult() {
+        handler(); //result assigns here if value for calculation is valid
+        return result;
+    }
+
+    //method for calculation
+    private int plus() {
+        if (valueForCalc == null || valueForCalc.equals("")) return 0;
+        String[] arrayOfNumbers = valueForCalc.split("[^-a-zA-Z\\d]");
         int sum = 0;
         for (String number : arrayOfNumbers) {
             int a = Integer.parseInt(number);
@@ -26,9 +54,10 @@ public class Calculator {
         return sum;
     }
 
-    public static void handlerAndPrintOut(String source) {
+    //handler of Exceptions
+    private void handler() {
         try {
-            System.out.println(plus(source));
+            result = plus();
         } catch (NumberFormatException e) {
             System.out.println("Input value is not a number");
         } catch (IllegalArgumentException e) {
@@ -36,4 +65,9 @@ public class Calculator {
         }
     }
 
+    @Override
+    public String toString() {
+        return "value for calculation='" + valueForCalc + '\'' +
+                ", result=" + result;
+    }
 }
--- src/test/java/TestCalc.java
+++ src/test/java/TestCalc.java
@@ -1,43 +1,37 @@
+import org.example.Calculator;
 import org.junit.jupiter.api.Test;
 
-import static org.example.Calculator.handlerAndPrintOut;
-import static org.example.Calculator.plus;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCalc {
 
-    @Test
-    public void nullCheckTest() {
-        assertEquals(0, plus(null));
-        assertEquals(0, plus(""));
-    }
 
     @Test
-    public void exceptionsTest() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            plus("0 -6 9 3");
-        });
-        assertThrows(NumberFormatException.class, () -> {
-            plus("5 gf");
-        });
+    public void nullCheckTest() {
+        assertEquals(0, new Calculator(null).getResult());
+        assertEquals(0, new Calculator("").getResult());
     }
 
     @Test
     public void handlerTest() {
-        handlerAndPrintOut("0 -6 9 3");
-        handlerAndPrintOut("5 gf");
+        Calculator calc1 = new Calculator("65 g3");
+        Calculator calc2 = new Calculator("-4 7");
+        assertEquals(0, calc1.getResult());
+        assertEquals(0, calc2.getResult());
     }
 
     @Test
     public void ignoreGreaterThan100Test() {
-        assertEquals(2 + 5 + 100, plus("2 100 5 300"));
+        assertEquals(2 + 5 + 100, new Calculator("2 100 5 300").getResult());
     }
 
+    //
     @Test
     public void differentDilimeterTest() {
-        assertEquals(plus("0 5 9 33"), plus("0,5,9,33"));
-        assertEquals(plus("0 5 9 33"), plus("0.5.9.33"));
-        assertEquals(plus("0 5 9 33"), plus("0/5/9/33"));
+        assertEquals(0 + 5 + 9 + 33, new Calculator("0,5,9,33").getResult());
+        assertEquals(0 + 5 + 9 + 33, new Calculator("0.5.9.33").getResult());
+        assertEquals(0 + 5 + 9 + 33, new Calculator("0.5/9/33").getResult());
     }
 
 }
